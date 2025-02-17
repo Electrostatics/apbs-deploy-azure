c9414984e61e09bf0e4798649817ab22834885ab
Ethan Holz <ethan.holz@omsf.io>
2025-02-17 14:13:01 -0700

    feat: add ACR name and RG name

---
+++ modules/apbs-backend/container-app/main.tf
@@ -0,0 +1,56 @@
+data "azurerm_container_registry" "acr" {
+  name                = var.registry_name
+  resource_group_name = var.registry_resource_group_name
+}
+
+resource "azurerm_user_assigned_identity" "container_app_identity" {
+  name                = "${var.app_name}-identity"
+  location            = var.location
+  resource_group_name = var.backend_resource_group_name
+}
+
+resource "azurerm_role_assignment" "container_app_role_assignment" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.container_app_identity.principal_id
+}
+
+resource "azurerm_container_app_environment" "app_env" {
+  name                = "${var.app_name}-env"
+  location            = var.location
+  resource_group_name = var.backend_resource_group_name
+}
+
+resource "azurerm_container_app_job" "app" {
+  name                         = var.app_name
+  resource_group_name          = var.backend_resource_group_name
+  location                     = var.location
+  container_app_environment_id = azurerm_container_app_environment.app_env.id
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.container_app_identity.id]
+  }
+
+  registry {
+    server   = data.azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.container_app_identity.id
+  }
+
+  replica_timeout_in_seconds = var.replica_timeout_in_seconds
+  replica_retry_limit        = 1
+
+  manual_trigger_config {
+    parallelism              = 1
+    replica_completion_count = 1
+  }
+
+  template {
+    container {
+      name   = "apbscontainer"
+      image  = "${data.azurerm_container_registry.acr.login_server}/${var.image_name}:${var.image_tag}"
+      cpu    = var.cpu
+      memory = var.memory
+    }
+  }
+}
---
+++ modules/apbs-backend/container-app/variables.tf
@@ -0,0 +1,50 @@
+variable "registry_name" {
+  description = "The name of the container registry"
+  type        = string
+}
+
+variable "registry_resource_group_name" {
+  description = "The name of the resource group containing the container registry"
+  type        = string
+}
+
+variable "app_name" {
+  description = "The name of the container app"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the container app"
+  type        = string
+}
+
+variable "backend_resource_group_name" {
+  description = "The name of the resource group containing the container app"
+  type        = string
+}
+
+variable "replica_timeout_in_seconds" {
+  description = "The timeout in seconds for the container app"
+  type        = number
+  default     = 600
+}
+
+variable "cpu" {
+  description = "The CPU for the container app"
+  type        = number
+}
+
+variable "memory" {
+  description = "The memory for the container app"
+  type        = string
+}
+
+variable "image_name" {
+  description = "The name of the container image"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "The tag of the container image"
+  type        = string
+}
