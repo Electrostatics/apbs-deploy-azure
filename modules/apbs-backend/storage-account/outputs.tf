644c2ad347d831787f1f9b38fd82126844a00801
Ethan Holz <ethan.holz@omsf.io>
2025-02-24 11:19:12 -0700

    fix: add the output endpoints for queue and blob

--- modules/apbs-backend/storage-account/outputs.tf
+++ modules/apbs-backend/storage-account/outputs.tf
@@ -4,6 +4,8 @@ output "storage_account" {
     primary_access_key        = azurerm_storage_account.storage.primary_access_key
     id                        = azurerm_storage_account.storage.id
     primary_connection_string = azurerm_storage_account.storage.primary_connection_string
+    primary_queue_endpoint    = azurerm_storage_account.storage.primary_queue_endpoint
+    primary_blob_endpoint     = azurerm_storage_account.storage.primary_blob_endpoint
   }
   sensitive = true
 }
