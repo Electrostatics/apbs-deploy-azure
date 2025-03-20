e566ead1bbe70edf8890dd846a359cb666e0ddfb
Ethan Holz <ethan.holz@omsf.io>
2025-03-20 11:49:26 -0600

    feat(registry): add auto image cleanup

--- modules/registry/main.tf
+++ modules/registry/main.tf
@@ -1,7 +1,34 @@
 resource "azurerm_container_registry" "registry" {
-  name                     = var.registry_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  sku                      = "Basic"
-  admin_enabled            = true
+  name                = var.registry_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_container_registry_task" "cleanup" {
+  name                  = "cleanup"
+  container_registry_id = azurerm_container_registry.registry.id
+  platform {
+    os = "Linux"
+  }
+  encoded_step {
+    task_content = yamlencode({
+      version = "v1.1.0"
+      steps = [
+        {
+          cmd                             = "acr purge --filter '${var.container_name}:${var.container_tag_regex}' --untagged --ago 0d --keep 5"
+          disableWorkingDirectoryOverride = true
+          timeout                         = 3600
+        }
+      ]
+    })
+    context_path = "/dev/null"
+  }
+
+  timer_trigger {
+    name     = "cleanup_timer"
+    schedule = var.cleanup_schedule
+    enabled  = true
+  }
 }
--- modules/registry/variables.tf
+++ modules/registry/variables.tf
@@ -12,3 +12,20 @@ variable "registry_name" {
   description = "The name of the container registry."
   type        = string
 }
+
+variable "container_name" {
+  description = "The name of the container to be cleaned up."
+  type        = string
+}
+
+variable "container_tag_regex" {
+  description = "The regex to match the container tags to be cleaned up."
+  type        = string
+  default     = ".*"
+}
+
+variable "cleanup_schedule" {
+  description = "The schedule for the cleanup task."
+  type        = string
+  default     = "0 0 * * 0"
+}
